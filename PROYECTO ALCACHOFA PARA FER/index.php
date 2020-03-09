641abf9ca3a39f5ebf5a83b3887c606cb6d34f0c
potchi23 <53357059+potchi23@users.noreply.github.com>
2020-03-09 13:28:38 +0100

    PROYECTO TAMBIEN PARA HAROLD

---
+++ PROYECTO ALCACHOFA PARA FER/index.php
@@ -0,0 +1,22 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>PÁGINA DE INICIO</title>
+</head>
+<body>
+    <header>
+        <?php include 'includes/comun/cabecera.php'?>
+    </header>
+
+    <div>
+        <p>En construcción...</p>
+    </div>
+
+    <footer>
+        <?php include 'includes/comun/pie.php'?>
+    </footer>
+</body>
+</html>
