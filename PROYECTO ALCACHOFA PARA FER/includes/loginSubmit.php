dfca8273fd253ff7f308f3a56c07cde5c27f8a68
potchi23 <53357059+potchi23@users.noreply.github.com>
2020-03-16 19:11:13 +0100

    Update loginSubmit.php

--- PROYECTO ALCACHOFA PARA FER/includes/loginSubmit.php
+++ PROYECTO ALCACHOFA PARA FER/includes/loginSubmit.php
@@ -1,25 +1,19 @@
 <?php
-
+include_once('MySQLConnection.php');
+include_once('UserDAO.php');
+include_once('User.php');
 session_start();
 
 $username = $_REQUEST["username"];
-$passwd = $_REQUEST["passwd"];
-$loginUsername="";
-$loginPasswd="";
-$name = "";
-include "mySQLConnection.php";
+$password = $_REQUEST["password"];
+
+$mysql = new MySQLConnection();
+$conn = $mysql->connect(); 
+$userDAO = new UserDAO();
 
 //OBTENEMOS USUARIO Y CONTRASEÑA DESDE LA BASE DE DATOS
-$loginUser = "SELECT username, password, name, img FROM user WHERE username =" ."'".$username."'";
-$result = $conn->query($loginUser);
-
-if ($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
-        $loginUsername = $row["username"];
-        $loginPasswd = $row["password"];
-        $name = $row["name"];
-        $blob = $row["img"];
-    }
+if ($userDAO->userExists($conn, $username)) {
+    $user = $userDAO->loginUser($conn, $username, $password, $name, $img);
 } 
 
 //ERROR CUANDO EL USUARIO NO ESTÁ REGISTRADO
@@ -29,17 +23,16 @@ else {
    
     header("Location: /login.php");
 }
-$conn->close();
 
 //ERROR CUANDO DEJAMOS EL FORMULARIO EN BLANCO
-if($username == "" || $passwd == ""){
+if($username == "" || $password == ""){
     $_SESSION["login"] = false;
     $_SESSION["userInDB"] = false;
     header("Location: /login.php");
 }
 
 //ERROR CUANDO EXISTE EL USUARIO Y LA CONTRASEÑA ES INCORRECTA
-else if($username == $loginUsername && $passwd != $loginPasswd){
+else if($username == $user->getUsername() && $password != $user->getPassword()){
     $_SESSION["login"]= false;
     $_SESSION["userInDB"] = true;
     $_SESSION["correctPass"] = false;
@@ -47,11 +40,12 @@ else if($username == $loginUsername && $passwd != $loginPasswd){
 }
 
 //ACCESO CUANDO EL USUARIO EXISTE Y LA CONTRASEÑA ES CORRECTA
-else if($username == $loginUsername && $passwd == $loginPasswd){
+else if($username == $user->getUsername() && $password == $user->getPassword()){
     $_SESSION["login"] = true;
     $_SESSION["userInDB"] = true;
     $_SESSION["correctPass"] = true;
-    $_SESSION["name"] = $name;
+    $_SESSION["name"] = $user->getName();
     header("Location: /index.php");
 }
 ?>
+
